88e2b7ef4869ea608166c73256212aca9f992834
Aakash Gajjar <aakash.g@codiste.com>
2024-06-05 15:37:21 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Tiptap from "#/app/components/Editor";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="App">
+        <Tiptap />
+      </div>
+    </main>
+  );
+}
